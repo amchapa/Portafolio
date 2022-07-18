aa0132a1992ae761d22242ed1ca022e783816a97
amchapa <angychapaprincipe@gmail.com>
2022-07-18 00:51:12 -0500

    Initial Structure : 4 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { HomeComponent } from './home/home.component';
+import { ToggleComponent } from './toggle/toggle.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, TopbarComponent, HomeComponent, ToggleComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
